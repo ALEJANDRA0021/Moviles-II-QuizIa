4807242e93adf43c2c0cb1086f226f6c952557e4
ale <alejandra.hernandez3@catolica.edu.sv>
2026-03-22 17:01:53 -0600

    Initial Commit

---
+++ Proyecto_QuizIa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proyecto_QuizIa
+//
+//  Created by macbook pro on 3/22/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
