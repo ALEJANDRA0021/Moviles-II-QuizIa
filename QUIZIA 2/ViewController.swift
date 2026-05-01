6532f48ad2d9731363e14601020c55d04db2a870
ale <alejandra.hernandez3@catolica.edu.sv>
2026-05-01 13:40:19 -0600

    Initial Commit

---
+++ QUIZIA 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QUIZIA 2
+//
+//  Created by macbook pro on 5/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
